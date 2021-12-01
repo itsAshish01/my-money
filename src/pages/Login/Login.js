9946bc00265c0f2d16a2542f4880be7b851074b2
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-01 14:08:04 +0530

    Build the login page.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import styles from './Login.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
   );
 };
 
