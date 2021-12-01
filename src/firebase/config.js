239dddd924fafb2c6809f02935bcde4adbd857c7
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-01 15:50:33 +0530

    Install & setup firebase.

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDuIowyZsBHngDpZKeJQzt2AoWmGEIQvA4',
+  authDomain: 'mymoney-byap.firebaseapp.com',
+  projectId: 'mymoney-byap',
+  storageBucket: 'mymoney-byap.appspot.com',
+  messagingSenderId: '1049239997989',
+  appId: '1:1049239997989:web:ed25be6d66c6a74c8039cc',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
