fbb22369a1483521710b9c01ed93188882da1f43
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-02 11:40:22 +0530

    Dispatching Login action.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,6 +4,9 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload };
+
     default:
       return state;
   }
@@ -11,6 +14,7 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { user: null });
+  console.log('AuthContext state: ', state);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -15,12 +17,15 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(res.user);
 
       if (!res) throw new Error('Could not signup user');
 
       // add display name to user
       await res.user.updateProfile({ displayName });
+
+      //dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
+
       setIsPending(false);
       setError(null);
     } catch (err) {
