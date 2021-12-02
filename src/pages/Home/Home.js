ccdda16038cc8493a92b733dbffedadc444f77de
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-02 18:08:49 +0530

    Create the transaction list component.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,17 @@
 import styles from './Home.module.css';
+import TransactionForm from './TransactionForm';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h3>Transaction List</h3>
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
