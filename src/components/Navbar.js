6dff3f208e284148eff653fdca8d616ebfe15e57
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-02 13:41:27 +0530

    Conditionally render Navbar links acc to user state.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to='/login'>Sign in</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Create account</Link>
-        </li>
-        <li>
-          <button className='btn' onClick={logout}>
-            Sign out
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Sign in</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Create account</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>hello, {user.displayName}</li>
+            <li>
+              <button className='btn' onClick={logout}>
+                Sign out
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
