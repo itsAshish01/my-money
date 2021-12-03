504fe5fde09ec1a42e844a69fc47a073e092a0f4
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-04 00:32:51 +0530

    Add Transactions to Home using TransactionList.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,18 @@
 import styles from './Home.module.css';
 import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 const Home = () => {
   const { user } = useAuthContext();
+  const { error, documents } = useCollection('transactions');
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h3>Transaction List</h3>
+        {error && <p className='error'>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
       </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
---
+++ src/pages/Home/TransactionList.js
@@ -0,0 +1,19 @@
+import styles from './Home.module.css';
+
+const TransactionList = ({ transactions }) => {
+  return (
+    <>
+      <h3>All Transactions</h3>
+      <ul className={styles.transactions}>
+        {transactions.map((transaction) => (
+          <li key={transaction.id}>
+            <p className={styles.name}>{transaction.name}</p>
+            <p className={styles.amount}>${transaction.amount}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
